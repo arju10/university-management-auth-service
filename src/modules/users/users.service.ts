96685528ac2ecb01a98a49589b39909ba8cfb398
arju10 <arjuarju71@gmail.com>
2023-06-26 08:58:54 +0600

    create user utils & Create generateUser utility function

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,8 +1,21 @@
 import { User } from "./users.model";
 import { IUser } from "./users.interface";
+import config from "../../config/index";
+import { generateUserId } from "./users.utils";
+
 
 const createUser = async (user: IUser): Promise<IUser | null> => {
     const createdUser = await User.create(user);
+
+    // Auto Generated Increment ID
+    const id = await generateUserId();
+
+    user.id = id;
+
+    // Default Password
+    if(!user.password){
+        user.password = config.default_user_pass as string
+    }
 
     if(!createUser){
         throw new Error("Failed to create user");
