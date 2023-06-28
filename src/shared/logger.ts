aed914e8bdc3c789af357b2292f5d9d63eda3418
arju10 <arjuarju71@gmail.com>
2023-06-28 18:51:39 +0600

    Manage Logs using a library named winston-daily-rotate-file

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -1,8 +1,10 @@
-import path from 'path';
-import { createLogger, format, transports } from 'winston';
-const { combine, timestamp, label, printf, prettyPrint } = format;
+import path from 'path'
+import { createLogger, format, transports } from 'winston'
+import DailyRotateFile from 'winston-daily-rotate-file'
+const { combine, timestamp, label, printf } = format
 
 //Custom Log Format
+
 const myFormat = printf(({ level, message, label, timestamp }) => {
   const date = new Date(timestamp)
   const hour = date.getHours()
@@ -13,44 +15,44 @@ const myFormat = printf(({ level, message, label, timestamp }) => {
 
 const logger = createLogger({
   level: 'info',
-  format:  combine(
-    label({ label: 'right meow!' }),
-    timestamp(),
-    myFormat,
-    prettyPrint(),
-  ),
-
+  format: combine(label({ label: 'PH' }), timestamp(), myFormat),
   transports: [
     new transports.Console(),
-    new transports.File({filename: path.join(
-      process.cwd(),
-      'logs',
-      'winston',
-      'successes',
-    ), level: 'info' })
- 
+    new DailyRotateFile({
+      filename: path.join(
+        process.cwd(),
+        'logs',
+        'winston',
+        'successes',
+        'phu-%DATE%-success.log'
+      ),
+      datePattern: 'YYYY-DD-MM-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
+    }),
   ],
-});
+})
 
 const errorlogger = createLogger({
   level: 'error',
-  format:  combine(
-    label({ label: 'right meow!' }),
-    timestamp(),
-    myFormat
-  ),
-
+  format: combine(label({ label: 'PH' }), timestamp(), myFormat),
   transports: [
     new transports.Console(),
-    new transports.File({filename: path.join(
-      process.cwd(),
-      'logs',
-      'winston',
-      'error.log',
-    ), level: 'error' })
- 
+    new DailyRotateFile({
+      filename: path.join(
+        process.cwd(),
+        'logs',
+        'winston',
+        'successes',
+        'phu-%DATE%-error.log'
+      ),
+      datePattern: 'YYYY-DD-MM-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
+    }),
   ],
-});
+})
 
-export { errorlogger, logger };
-
+export { errorlogger, logger }
