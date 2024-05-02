eae30584a8f9cf15c771adaf38a7610c7c65d972
arju10 <arjuarju71@gmail.com>
2024-05-02 15:35:25 +0600

    Optimize Pagination Query Object Using Pick

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -3,6 +3,8 @@ import { AcademicSemesterService } from './academicSemester.service';
 import catchAsync from '../../../shared/catchAsync';
 import sendResponse from '../../../shared/sendResponse';
 import httpStatus from 'http-status';
+import pick from '../../../shared/pick';
+import { paginationField } from '../../../constants/pagination';
 
 const createSemester: RequestHandler = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -28,19 +30,20 @@ const createSemester: RequestHandler = catchAsync(
 
 const getAllSemesters = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    const paginationOptions = {
-      page: Number(req.query.page),
-      limit: Number(req.query.limit),
-      sortBy: req.query.sortBy,
-      sortOrder: req.query.sortOrder,
-    };
-
+    // const paginationOptions = {
+    //   page: Number(req.query.page),
+    //   limit: Number(req.query.limit),
+    //   sortBy: req.query.sortBy,
+    //   sortOrder: req.query.sortOrder,
+    // };
+    const paginationOptions = pick(req.query, paginationField);
+    console.log(paginationOptions);
     const result = await AcademicSemesterService.getAllSemesters({});
 
     sendResponse(res, {
       statusCode: httpStatus.OK,
       success: true,
-      message: 'Academic Semester is created successfully',
+      message: 'Data is retrieved successfully',
       data: result,
     });
   },
