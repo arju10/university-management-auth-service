5c0f9b94f8c93cbda352193709d752359332fc5c
arju10 <arjuarju71@gmail.com>
2024-05-04 09:47:31 +0600

    Create Faculty Model

--- src/app/modules/academicFaculty/academicFaculty.interface.ts
+++ src/app/modules/academicFaculty/academicFaculty.interface.ts
@@ -4,7 +4,7 @@ export type IAcademicFaculty = {
   title: string;
 };
 
-export type AcademicSemesterModel = Model<
+export type AcademicFacultyModel = Model<
   IAcademicFaculty,
   Record<string, unknown>
 >;
