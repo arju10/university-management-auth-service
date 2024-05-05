9d32b5a9d74019f7b6cc308e8423a767d9b63cc9
arju10 <arjuarju71@gmail.com>
2024-05-05 17:02:48 +0600

    [DONE]Academic Department :last implementation  Zod Validation , Academic Department Validation

--- src/app/modules/academicDepartment/academicDepartment.route.ts
+++ src/app/modules/academicDepartment/academicDepartment.route.ts
@@ -1,16 +1,27 @@
 import express from 'express';
 
 import { AcademicDepartmentController } from './academicDepartment.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import { AcademicDepartmentValidation } from './academicDepartment.validation';
 
 const router = express.Router();
 
 router.post(
   '/create-department',
+  validateRequest(
+    AcademicDepartmentValidation.createAcademicDepartmentZodSchema,
+  ),
   AcademicDepartmentController.createDepartment,
 );
 router.get('/:id', AcademicDepartmentController.getSingleDepartment);
 
-router.patch('/:id', AcademicDepartmentController.updateDepartment);
+router.patch(
+  '/:id',
+  validateRequest(
+    AcademicDepartmentValidation.updateAcademicDepartmentZodSchema,
+  ),
+  AcademicDepartmentController.updateDepartment,
+);
 
 router.delete('/:id', AcademicDepartmentController.deleteDepartment);
 
